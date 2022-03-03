d58da30f004c3b178d80f1ec87f43d4a225a8c82
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-03-03 15:13:59 +0530

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -3,8 +3,7 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 
 let sk = Config.WORKTYPE == 'public' ? false : true
-skl.addCommand({pattern: 'url ?(.*)', fromMe: sk}, async (message, match) => {
-
-    await message.sendMessage('Use this site to get URL 👇:\n\n https://souravkl11.github.io/image-uploader/')
-
-        });
+e.addCommand({pattern: 'url ?(.*)', fromMe: a, desc:'Uploads image to imgur.com'}, (async (m, match) => { 
+var q = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
+var res = await i.query.upload(q,v)
+await m.client.sendMessage(m.jid, '_*File uploaded!*_ \n\n_File name:_ ' + res.id + '.jpeg \n_File URL:_ ' + res.link + '\n_File size:_ ' + res.size + ' bits' + '\n_Format:_ ' + res.type + '\n_Height:_ ' + res.height + '\n_Width:_ ' + res.width , MessageType.text, { quoted: m.data });}));
