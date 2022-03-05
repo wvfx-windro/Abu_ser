d72c9f4501cabcb980bbb069f5d03685f22fafa3
wvfx-windro <100778740+wvfx-windro@users.noreply.github.com>
2022-03-05 17:34:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBU
 module.exports = {
     VERSION: 'v7.3.6',
     CHANNEL: 'https://t.me/remasterplugin',
-    SESSION: process.env.ABU_SER_CODE === undefined ? '' : process.env.ABU_SER_CODE,
+    SESSION: process.env.ITACHI_CODE === undefined ? '' : process.env.ITACHI_CODE,
     ANTİLİNK: process.env.ANTİ_LİNK === undefined ? 'false' : process.env.ANTİ_LİNK,
     AUTOBİO: process.env.AUTO_BİO === undefined ? 'false' : process.env.AUTO_BİO,
     GANSTYLE: process.env.GAN_IMAGE === undefined ? 'https://i.hizliresim.com/loUtAb.jpg' : process.env.GAN_IMAGE,
